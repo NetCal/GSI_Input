7b5d8c03205dba111616d0a90a0ffcdab4c1b4db
Malte Schütze <malte.schuetze@fgms.de>
2017-05-15 22:13:41 +0200

    Concave hull segment calculation up to last segment

--- src/main/java/unikl/disco/ProtocolSpec.java
+++ src/main/java/unikl/disco/ProtocolSpec.java
@@ -3,6 +3,8 @@ package unikl.disco;
 import unikl.disco.curves.ArrivalCurve;
 import unikl.disco.curves.Curve;
 import unikl.disco.curves.LinearSegment;
+import unikl.disco.numbers.Num;
+import unikl.disco.numbers.NumFactory;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -40,6 +42,42 @@ public class ProtocolSpec {
         return new ArrivalCurve(curve);
     }
 
+    private Num slope(int from, int to) {
+        return NumFactory.create(maxTrafficInInterval(to) - maxTrafficInInterval(from), to - from);
+    }
+
+    private List<Integer> getInitialConcaveHullSegments(int segmentEnd, Num minSlope) {
+        if (segmentEnd == 1) {
+            ArrayList<Integer> result = new ArrayList();
+            result.add(0); // Initial segment starts at 0
+            return result;
+        }
+
+        int segmentStart = segmentEnd - 1;
+        Num currentSlope = slope(segmentStart, segmentEnd);
+        Num maxSlope = currentSlope;
+
+        // Once we fall below min slope, we know we can't find a concave hull with the current configuration
+        while (currentSlope.geq(minSlope)) {
+
+            // Can't grow in slope without intercepting curve
+            if (currentSlope.leq(maxSlope)) {
+                List<Integer> segments = getInitialConcaveHullSegments(segmentStart, currentSlope);
+                if (segments != null) {
+                    segments.add(segmentStart);
+                }
+            }
+
+            segmentStart -= 1;
+            if (currentSlope.leq(maxSlope)) {
+                maxSlope = currentSlope;
+            }
+            currentSlope = slope(segmentStart, segmentEnd);
+        }
+
+        return null;
+    }
+
     public void add(Message message) {
         if (message.transmissionTime < 0 || message.transmissionTime >= cycleLength) {
             throw new IllegalArgumentException("Transmission time not in interval [0, cycle-length - 1]");
@@ -64,7 +102,6 @@ public class ProtocolSpec {
         }
     }
 
-    // TODO caching
     int maxTrafficInInterval(int intervalLength) {
         if (intervalLength >= 2 * cycleLength) {
             int fullCycles = (intervalLength / cycleLength) - 1;
