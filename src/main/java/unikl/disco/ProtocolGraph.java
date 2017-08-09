f9fbfe907c758672f1ce6443e192f4f36b6ee552
Malte Schütze <malte.schuetze@fgms.de>
2017-08-09 13:57:36 +0200

    Progress information for precalculation

--- src/main/java/unikl/disco/ProtocolGraph.java
+++ src/main/java/unikl/disco/ProtocolGraph.java
@@ -41,7 +41,28 @@ public class ProtocolGraph {
                 .orElse(0); // Max returns none if there are no blocks, therefore no traffic
     }
 
+    private long longestBlockLength() {
+        return blocks.values().stream()
+                .mapToLong(Block::getPeriod)
+                .max()
+                .orElse(0); // If no blocks, 0 length of longest block
+    }
+
     public PseudoPeriodicFunction approximateSubadditive(long k) {
+        long precompLimit = k + longestBlockLength();
+        long milestonePeriod = precompLimit / 10000;
+
+        for (long i = 0; i < precompLimit; i++) {
+            if (i % milestonePeriod == 0) {
+                System.out.printf("Precalculating values %3.2f%% (%d/%d)%n",
+                        i / milestonePeriod / 100.0, i, precompLimit);
+            }
+            for (Block block: blocks.values()) {
+                block.precalculateMaxPrefix(i);
+            }
+        }
+        System.out.printf("\rPrecalculating values 100.00%% (%d/%d)%n", precompLimit, precompLimit);
+
         PseudoPeriodicFunction result = new PseudoPeriodicFunction(0, k, maxTraffic(k));
         for (long i = 0; i < k; i++) {
             if (i % (k/100) == 0) {
